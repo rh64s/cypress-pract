15e6179a0458de799a6e4358f873ae0b66ed9de0
rh64s <cchelovekov64@gmail.com>
2026-04-22 16:15:38 +0700

    test-case: complete

---
+++ cypress/e2e/registration.cy.js
@@ -0,0 +1,51 @@
+import RandHelper from "../../modules/helpers.js";
+
+describe('register', () => {
+    it('register', () => {
+        cy.visit('https://dev.profteam.su/registration');
+        cy.wait(3000);
+        let randomizedUsername = RandHelper.randomCharacters(30) + "user" + RandHelper.randomCharacters(5);
+        cy.get(':nth-child(1) > :nth-child(1) > .form-control--medium > .form-input--text').type(randomizedUsername);
+        cy.get(':nth-child(1) > :nth-child(1) > .form-control--medium > .form-input--text').should('have.value', randomizedUsername);
+
+        let randomizedEmail = randomizedUsername + "email@example.com"
+        cy.get('.form-input--email', {timeout: 4000}).type(randomizedEmail);
+        cy.get('.form-input--email').should('have.value', randomizedEmail);
+
+        let randomizedPassword = "Password1!"
+        cy.get(':nth-child(3) > .form-control--medium > .form-input--password', {timeout: 4000}).type(randomizedPassword);
+        cy.get(':nth-child(3) > .form-control--medium > .form-input--password').should('have.value', randomizedPassword);
+
+        cy.get(':nth-child(4) > .form-control--medium > .form-input--password', {timeout: 4000}).type(randomizedPassword);
+        cy.get(':nth-child(4) > .form-control--medium > .form-input--password').should('have.value', randomizedPassword);
+
+        cy.get(':nth-child(4) > .button').click();
+        cy.wait(3000);
+
+        cy.get('[style=""] > :nth-child(1) > .form-control--medium > .form-input--text').type("рандом")
+        cy.get(':nth-child(2) > .form-control--medium > .form-input--text').type("рандом")
+        cy.get(':nth-child(3) > .form-control--medium > .form-input--text').type("рандом")
+
+        cy.get('[style=""] > :nth-child(1) > .form-control--medium > .form-input--text').should('have.value', "рандом")
+        cy.get(':nth-child(2) > .form-control--medium > .form-input--text').should('have.value', "рандом")
+        cy.get(':nth-child(3) > .form-control--medium > .form-input--text').should('have.value', "рандом")
+        cy.get(':nth-child(3) > .button').click();
+    });
+
+    it('register validation check on 1 part', () => {
+        cy.visit('https://dev.profteam.su/registration');
+        cy.wait(3000);
+
+        cy.get(':nth-child(1) > :nth-child(1) > .form-control--medium > .form-input--text').type('русские букавы');
+        cy.get('.form-input--email', {timeout: 4000}).type('asdfпросторандом');
+        cy.get(':nth-child(3) > .form-control--medium > .form-input--password', {timeout: 4000}).type('хихах');
+        cy.get(':nth-child(4) > .form-control--medium > .form-input--password', {timeout: 4000}).type('хахих');
+
+        cy.wait(1000);
+
+        cy.contains('.form-error > span', 'Обязательное поле, символы латиницы, не содержит пробелы').should('exist')
+        cy.contains('.form-error > span', 'Обязательное поле, некорректная почта').should('exist')
+        cy.contains('.form-error > span', 'Обязательное поле, мин 6 символов, должен содержать буквы в верхнем и нижнем регистре, минимум 1 цифру, не содержать пробелы').should('exist')
+        cy.contains('.form-error > span', 'Пароли не совпадают').should('exist')
+    });
+})
--- modules/helpers.js
+++ modules/helpers.js
@@ -1,16 +1,16 @@
-class RandHelper {
+export default class RandHelper {
 
-    alphabet = ['a', 'b', 'c', 'd', 'e'];
+    static alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'];
 
     static randomCharacters(length) {
         let answer = "";
         for (let l = 0; l < length; l++) {
-            answer = self.alphabet[this.rand() % 5];
+            answer += this.alphabet[(this.randomInt() + l) % this.alphabet.length-1];
         }
         return answer;
     }
 
-    static rand() {
-        return Date.now();
+    static randomInt() {
+        return Math.floor(Date.now() / 1000);
     }
 }
