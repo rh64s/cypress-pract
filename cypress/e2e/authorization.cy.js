4254e2c8d95aee084e4675e96b61cf295c39125d
rh64s <cchelovekov64@gmail.com>
2026-04-22 23:11:13 +0700

    add: authorization not passed

--- cypress/e2e/authorization.cy.js
+++ cypress/e2e/authorization.cy.js
@@ -1,5 +1,5 @@
 describe('register', () => {
-    it('authorization', () => {
+    it('authorization can login', () => {
         cy.fixture('authorization/authorization.json').as('data');
         cy.visit('https://dev.profteam.su/login');
         cy.wait(500);
@@ -14,4 +14,19 @@ describe('register', () => {
 
         cy.get('.form__buttons > :nth-child(3) > button').click();
     });
+
+    it('authorization wrong login', () => {
+        cy.fixture('authorization/authorizationWrong.json').as('data');
+        cy.visit('https://dev.profteam.su/login');
+        cy.wait(500);
+
+        cy.get("@data").then((data) => {
+            cy.get('.form-input--text').type(data.login);
+            cy.get('.form-input--password').type(data.password);
+        });
+
+        cy.get('.form__buttons > :nth-child(3) > button').click();
+
+        cy.contains('.form-error > span', 'Неверный логин или пароль, попробуйте заново.').should('exist')
+    })
 });
